ed3c2c5bf3e000a136631c90ac7559e08b0899f1
uche <uchesoranye@gmail.com>
2024-01-20 16:17:21 -0700

    connect to mongo, get data from db

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const router = new express.Router() 
-const routeController = require('../controllers')
+const express = require('express');
+const router = express.Router();
+const contactRoute = require('./contactsRoute'); 
+const routeController = require('../controllers');
 
 router.get("/", routeController.jackRoute);
 router.get("/spa", routeController.sparrowRoute);
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const { initDb } = require('./mongoDb/mongodb.js');
 
-app.use('/', require('./routes/routes.js'))
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`web server started on port ${port} `)
-})
+app.use('/', require('./routes/routes.js'));
+app.use('/', require('./routes/contactsRoute.js'));
+
+initDb((err) => {
+    if (err) {
+        console.error('Error initializing MongoDB:', err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
+});
