b0932d097103770b5b3ba4390c29c6f8a6e1a8b0
ucheoranye <uchesoranye@gmail>
2024-01-30 14:52:28 -0700

    fixed all

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const { initDb } = require('./mongoDb/mongodb.js');
+const router = require('./routes/routes.js')
 
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use('/', require('./routes/routes.js'));
+app.use('/', router);
 app.use('/', require('./routes/contactsRoute.js'));
 app.use('/', require('./routes/CRUDRoutes.js'));
 
